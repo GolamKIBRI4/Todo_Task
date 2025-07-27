74a5a83cbcf44a332595586b059cbf3a7832e464
GolanKIBRI4 <gk599625@gmail.com>
2025-07-27 16:50:53 +0600

    login page intro text section added

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -18,22 +18,34 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="login-div" style={{ marginTop: '150px' }}>
-        <h2 >Login</h2>
-      <form onSubmit={handleLogin} className='login-form'>
+    <>
+        <div className='login_text_div'>
+          <h2 style={{ color: "white",fontSize:"40px" }}>Welcome to Todo App</h2>
+                     <div className='login_text'>
+                         <p style={{color:"white"}}>be productive</p>
+                                   <p style={{color:"white"}}>You can manage your tasks here   </p>
+                     </div>
+          
+
+        </div>
+        <div className="login-div">
+            <h2 >Login</h2>
+          <form onSubmit={handleLogin} className='login-form'>
         
-        <input
-          type="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          placeholder="Enter your email"
-         
-        />
-        <button type="submit" className='login-form-button' >
-          Login
-        </button>
-      </form>
-    </div>
+            <input
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              placeholder="Enter your email"
+        
+            />
+            <button type="submit" className='login-form-button' >
+              Login
+            </button>
+          </form>
+        </div>
+        </>
+    
   );
 }
 
