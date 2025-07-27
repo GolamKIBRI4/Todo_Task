692914edecb883d4795457b2138bf01d229c1bff
GolanKIBRI4 <gk599625@gmail.com>
2025-07-27 17:00:32 +0600

    login button warning toast added

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,54 +1,50 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { login } from '../features/auth/authSlice';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../features/auth/authSlice";
+import {toast} from "sonner";
+
 
 const LoginForm = () => {
-
-      const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!email) return alert('Please enter email');
-    const fakeToken = 'fake-token-123';
+    if (!email) return toast.warning("Please enter your email");
+    const fakeToken = "fake-token-123";
     dispatch(login(fakeToken));
-    navigate('/userhome');
+    navigate("/userhome");
   };
 
   return (
     <>
-        <div className='login_text_div'>
-          <h2 style={{ color: "white",fontSize:"40px" }}>Welcome to Todo App</h2>
-                     <div className='login_text'>
-                         <p style={{color:"white"}}>be productive</p>
-                                   <p style={{color:"white"}}>You can manage your tasks here   </p>
-                     </div>
-          
-
+      <div className="login_text_div">
+        <h2 style={{ color: "white", fontSize: "40px" }}>
+          Welcome to Todo App
+        </h2>
+        <div className="login_text">
+          <p style={{ color: "white" }}>be productive</p>
+          <p style={{ color: "white" }}>You can manage your tasks here </p>
         </div>
-        <div className="login-div">
-            <h2 >Login</h2>
-          <form onSubmit={handleLogin} className='login-form'>
-        
-            <input
-              type="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              placeholder="Enter your email"
-        
-            />
-            <button type="submit" className='login-form-button' >
-              Login
-            </button>
-          </form>
-        </div>
-        </>
-    
+      </div>
+      <div className="login-div">
+        <h2>Login</h2>
+        <form onSubmit={handleLogin} className="login-form">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+          />
+          <button type="submit" className="login-form-button">
+            Login
+          </button>
+        </form>
+      </div>
+    </>
   );
-}
+};
 
-export default LoginForm
-
-
+export default LoginForm;
